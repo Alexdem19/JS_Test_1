f23abdd3718c847c90beadf31965f66c167ddd60
Alexdem19 <ad07217@yandex.ru>
2019-11-29 14:27:30 +0500

    Это первый commit

---
+++ Test_1/my_js.js
@@ -0,0 +1,14 @@
+var one = 'HELLO_+JQuery__??!!!!'
+
+
+console.log(one)
+//document.getElementById('message').innerHTML = one;
+//alert('Hello!!!!?');
+
+$('#test').text(one);
+$('#test').fadeOut("slow");
+
+
+//$('.myclass').text(one);
+
+document.getElementsByClassName("myclass")[0].innerHTML = one;
